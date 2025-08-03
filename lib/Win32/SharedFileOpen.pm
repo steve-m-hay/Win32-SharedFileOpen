0d73af84c8257c0149e64592c69cab65e0786451
Steve Hay <steve.m.hay@googlemail.com>
2025-08-03 16:14:25 +0100

    Fix broken rt.cpan.org link

--- lib/Win32/SharedFileOpen.pm
+++ lib/Win32/SharedFileOpen.pm
@@ -1540,7 +1540,7 @@ Bug reports and suggestions can be made on the CPAN Request Tracker at
 L<https://rt.cpan.org/Public/Bug/Report.html?Queue=Win32-SharedFileOpen>.
 
 Currently active requests on the CPAN Request Tracker can be viewed at
-L<https://rt.cpan.org/Public/Dist/Display.html?Status=Active;Queue=Win32-SharedFileOpen>.
+L<https://rt.cpan.org/Public/Dist/Display.html?Queue=Win32-SharedFileOpen>.
 
 Please test this distribution.  See CPAN Testers Reports at
 L<https://www.cpantesters.org/> for details of how to get involved.
